a7e3c374d152a94beea8d66eea08c642ac52a93e
Ian Tan <ian@mmu.edu.my>
2016-04-28 11:25:38 +0800

    CY copy

---
+++ app/src/main/java/com/itex/ian/itex/MainActivity.java
@@ -0,0 +1,123 @@
+package com.itex.ian.itex;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.app.Activity;
+import android.hardware.SensorManager;
+
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Locale;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.widget.Toast;
+/*
+To test using
+"signature":"bab58551c9a76ffccb9b48174295471e"
+http://54.213.118.45/beam/web_service/country/list
+
+ */
+public class MainActivity extends AppCompatActivity {
+//    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE"; // Hmmmm
+    TextToSpeech ttsobj;    // TTS Object
+    String hostName;
+    float speechRate;   // setSpeechRate(float speechRate)
+    int localeLanguage; // ttsobj.setLanguage(Locale.UK); 1 = US, 0 = UK
+//    EditText ed1;       // Test to enter text for TTS
+    Button btnStart;    // This is to start/stop the playback (main button on the front page of the app)
+
+    public void settingsActivity(View view) {
+        Intent intent = new Intent(this, settingsActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.editText);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                settingsActivity(view);
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+            }
+        });
+
+        btnStart=(Button)findViewById(R.id.btnStart);
+
+
+        ed1=(EditText)findViewById(R.id.editText);
+        // b1=(Button)findViewById(R.id.button);
+
+        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String toSpeak = ed1.getText().toString();
+                Toast.makeText(getApplicationContext(), toSpeak, Toast.LENGTH_SHORT).show();
+                t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+    }
+
+    public void onPause(){
+        if(t1 !=null){
+            t1.stop();
+            t1.shutdown();
+        }
+        super.onPause();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
